cb76ed9054789f0a14a596605989b9a730cacc47
trevor <me@trevorbehnke.com>
2021-10-26 16:15:19 -0700

    chores

--- src/data.js
+++ src/data.js
@@ -102,5 +102,133 @@ export const shortcuts = [
 	{
 		task: 'Toggle word wrap',
 		shortcut: '⌥Z'
+	},
+	{
+		task: 'Insert cursor',
+		shortcut: '⌥ + click'
+	},
+	{
+		task: 'Insert cursor above',
+		shortcut: '⌥⌘↑'
+	},
+	{
+		task: 'Insert cursor below',
+		shortcut: '⌥⌘↓'
+	},
+	{
+		task: 'Undo last cursor operation',
+		shortcut: '⌘U'
+	},
+	{
+		task: 'Insert cursor at end of each line selected',
+		shortcut: '⇧⌥I'
+	},
+	{
+		task: 'Select current line',
+		shortcut: '⌘L'
+	},
+	{
+		task: 'Select all occurrences of current selection',
+		shortcut: '⇧⌘L'
+	},
+	{
+		task: 'Select all occurrences of current word',
+		shortcut: '⌘F2'
+	},
+	{
+		task: 'Expand / shrink selection',
+		shortcut: '⌃⇧⌘→ / ←'
+	},
+	{
+		task: 'Column (box) selection',
+		shortcut: '⇧⌥ + drag mouse'
+	},
+	{
+		task: 'Column (box) selection up/down',
+		shortcut: '⇧⌥⌘↑ / ↓'
+	},
+	{
+		task: 'Column (box) selection left/right',
+		shortcut: '⇧⌥⌘← / →'
+	},
+	{
+		task: 'Column (box) selection page up',
+		shortcut: '⇧⌥⌘PgUp'
+	},
+	{
+		task: 'Column (box) selection page down',
+		shortcut: '⇧⌥⌘PgDn'
+	},
+	{
+		task: 'Find',
+		shortcut: '⌘F'
+	},
+	{
+		task: 'Replace',
+		shortcut: '⌥⌘F'
+	},
+	{
+		task: 'Find next/previous',
+		shortcut: '⌘G / ⇧⌘G'
+	},
+	{
+		task: 'Select all occurrences of Find match',
+		shortcut: '⌥Enter'
+	},
+	{
+		task: 'Add selection to next Find match',
+		shortcut: '⌘D'
+	},
+	{
+		task: 'Move last selection to next Find match',
+		shortcut: '⌘K ⌘D'
+	},
+	{
+		task: 'Trigger suggestion',
+		shortcut: '⌃Space'
+	},
+	{
+		task: 'Trigger parameter hints',
+		shortcut: '⇧⌘Space'
+	},
+	{
+		task: 'Format document',
+		shortcut: '⇧⌥F'
+	},
+	{
+		task: 'Format selection',
+		shortcut: '⌘K ⌘F'
+	},
+	{
+		task: 'Go to Definition',
+		shortcut: 'F12'
+	},
+	{
+		task: 'Peek Definition',
+		shortcut: '⌥F12'
+	},
+	{
+		task: 'Open Definition to the side',
+		shortcut: '⌘K F12'
+	},
+	{
+		task: 'Quick Fix',
+		shortcut: '⌘.'
+	},
+	{
+		task: 'Show References',
+		shortcut: '⇧F12'
+	},
+	{
+		task: 'Rename Symbol',
+		shortcut: 'F2'
+	},
+	{
+		task: 'Trim trailing whitespace',
+		shortcut: '⌘K ⌘X'
+	},
+	{
+		task: 'Change file language',
+		shortcut: '⌘K M'
 	}
 ];
