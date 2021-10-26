700872d16254a94dd28188f6d7db499a1e7fa369
trevor <me@trevorbehnke.com>
2021-10-26 16:04:35 -0700

    chores

--- src/data.js
+++ src/data.js
@@ -22,5 +22,85 @@ export const shortcuts = [
 	{
 		task: 'Keyboard Shortcuts',
 		shortcut: '⌘K ⌘S'
+	},
+	{
+		task: 'Cut line (empty selection)',
+		shortcut: '⌘X'
+	},
+	{
+		task: 'Copy line (empty selection)',
+		shortcut: '⌘C'
+	},
+	{
+		task: 'Move line down/up',
+		shortcut: '⌥↓ / ⌥↑'
+	},
+	{
+		task: 'Copy line down/up',
+		shortcut: '⇧⌥↓ / ⇧⌥↑'
+	},
+	{
+		task: 'Delete line',
+		shortcut: '⇧⌘K'
+	},
+	{
+		task: 'Insert line below/above',
+		shortcut: '⌘Enter / ⇧⌘Enter'
+	},
+	{
+		task: 'Jump to matching bracket',
+		shortcut: '⇧⌘\\'
+	},
+	{
+		task: 'Indent/outdent line',
+		shortcut: '⌘] / ⌘['
+	},
+	{
+		task: 'Go to beginning/end of line',
+		shortcut: 'Home / End'
+	},
+	{
+		task: 'Go to beginning/end of file',
+		shortcut: '⌘↑ / ⌘↓'
+	},
+	{
+		task: 'Scroll line up/down',
+		shortcut: '⌃PgUp / ⌃PgDn'
+	},
+	{
+		task: 'Scroll page up/down',
+		shortcut: '⌘PgUp /⌘PgDn'
+	},
+	{
+		task: 'Fold/unfold region',
+		shortcut: '⌥⌘[ / ⌥⌘]'
+	},
+	{
+		task: 'Fold/unfold all subregions',
+		shortcut: '⌘K⌘[ / ⌘K⌘]'
+	},
+	{
+		task: 'Fold/unfold all regions',
+		shortcut: '⌘K⌘0 / ⌘K⌘J'
+	},
+	{
+		task: 'Add line comment',
+		shortcut: '⌘K ⌘C'
+	},
+	{
+		task: 'Remove line comment',
+		shortcut: '⌘K ⌘U'
+	},
+	{
+		task: 'Toggle line comment',
+		shortcut: '⌘/'
+	},
+	{
+		task: 'Toggle block comment',
+		shortcut: '⇧⌥A'
+	},
+	{
+		task: 'Toggle word wrap',
+		shortcut: '⌥Z'
 	}
 ];
